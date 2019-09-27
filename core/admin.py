a327c4b77ddfe643e59557c34135f0b1785dbed7
Mikhail Pyrev <m.pyryev@dev.kokoc.com>
2019-09-27 16:05:57 +0300

    Update CompanyAdmin

--- core/admin.py
+++ core/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
 
 from core.models import Company, Employee
 
@@ -13,3 +14,13 @@ class EmployeeInline(admin.TabularInline):
 @admin.register(Company)
 class CompanyAdmin(admin.ModelAdmin):
     inlines = [EmployeeInline]
+    list_display = ['__str__', 'invite_token', 'owner', 'total_member_count', 'online_member_count']
+    list_filter = ['owner']
+
+    def total_member_count(self, obj):
+        return obj.employee_set.count()
+    total_member_count.short_description = _('Total member count')
+
+    def online_member_count(self, obj):
+        return obj.employee_set.filter(state=Employee.State.online).count()
+    online_member_count.short_description = _('Online member count')
